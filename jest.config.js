9a7df09b3fb6d2a740e547b83e9212e697e7b52a
Sofi Tohme <sofia.tohme@globant.com>
2023-04-19 11:05:21 -0300

    [ADD] coverage config

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,22 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/fileTransformer.js',
   },
-};
+  collectCoverageFrom: [
+    'src/components/**/*.ts',
+    'src/components/**/*.tsx',
+    'src/containers/**/*.ts',
+    'src/containers/**/*.tsx',
+    'src/helpers/*.ts',
+    'src/helpers/*.js',
+    'src/reducers/*.ts',
+    'src/reducers/*.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+}
