cf3963b9aa1542a68d022f142c9a3194fac18211
Sofi Tohme <sofia.tohme@globant.com>
2023-04-19 10:55:08 -0300

    [ADD] ColorSelector test

--- src/components/ColorSelector/ColorSelector.test.tsx
+++ src/components/ColorSelector/ColorSelector.test.tsx
@@ -1,12 +1,29 @@
-import ColorSelector from "./index";
-import React from "react";
-import { screen, render } from "@testing-library/react";
-import './styles.scss';
+import ColorSelector from './index'
+import React from 'react'
+import { screen, render, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import './styles.scss'
 
-const mockFunction = jest.fn();
-describe("ColorSelector", () =>{
-    it('should render the ColorSelector on screen', ()=>{
-        render (<ColorSelector activeColor='#cbe86b' onSaveColor={mockFunction}/>);
-        expect(screen.getAllByRole(/data-color/i)).toHaveLength(5);
+const mockFunction = jest.fn()
+describe('ColorSelector', () => {
+  describe('when rendering default', () => {
+    it('should render the ColorSelector on screen', () => {
+      render(<ColorSelector activeColor="#cbe86b" onSaveColor={mockFunction} />)
+      expect(screen.getAllByRole(/data-color/i)).toHaveLength(5)
     })
+  })
+  describe('when clicking on a colour', () => {
+    it('shoud call onSaveColor function', async () => {
+      render(<ColorSelector activeColor="#cbe86b" onSaveColor={mockFunction} />)
+      // @ts-ignore
+      const colorBlue: HTMLBodyElement = document.querySelector(
+        'span[data-color="#80d4f6"]'
+      )
+      await userEvent.click(colorBlue);
+      await waitFor(() => {
+        expect(mockFunction).toHaveBeenCalledTimes(1)
+        expect(mockFunction).toHaveBeenNthCalledWith(1, '#80d4f6')
+      })
+    })
+  })
 })
